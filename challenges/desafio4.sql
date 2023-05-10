913bf7a884029388edff72ccf1d251a6aea9efc1
Larissa Simões <larissacarneiro2@hotmail.com>
2023-05-10 09:18:55 -0300

    Req 4

---
+++ challenges/desafio4.sql
@@ -0,0 +1,11 @@
+SELECT
+usuario.usuario_nome AS pessoa_usuaria,
+CASE
+WHEN YEAR(MAX(historico_reproducao.data_de_reproducao)) >= 2021 THEN 'Ativa'
+ELSE 'Inativa'
+END AS status_pessoa_usuaria
+FROM SpotifyClone.usuario
+LEFT JOIN SpotifyClone.historico_reproducao ON usuario.id = historico_reproducao.usuario_id
+GROUP BY usuario.id
+HAVING MAX(historico_reproducao.data_de_reproducao) IS NOT NULL
+ORDER BY pessoa_usuaria ASC;
