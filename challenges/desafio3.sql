49426568995022e1ac32551d25504697e86b8659
Larissa Simões <larissacarneiro2@hotmail.com>
2023-05-10 08:59:32 -0300

    Colocando os alias

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,8 +1,8 @@
 SELECT u.usuario_nome AS pessoa_usuaria,
 COUNT(hr.musica_id) AS musicas_ouvidas,
 ROUND(SUM(m.duracao)/60, 2) AS total_minutos
-FROM SpotifyClone.usuario u
-LEFT JOIN SpotifyClone.historico_reproducao hr ON u.id = hr.usuario_id
-LEFT JOIN SpotifyClone.musica m ON hr.musica_id = m.id
+FROM SpotifyClone.usuario AS u
+LEFT JOIN SpotifyClone.historico_reproducao AS hr ON u.id = hr.usuario_id
+LEFT JOIN SpotifyClone.musica AS m ON hr.musica_id = m.id
 GROUP BY u.usuario_nome
 ORDER BY u.usuario_nome ASC;
